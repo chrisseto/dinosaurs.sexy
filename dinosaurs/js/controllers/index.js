c2ce3a82155cdd03822a8359e35c68c896975fad
Chris Seto <chriskseto@gmail.com>
2014-10-19 22:52:18 -0400

    Add a method for requesting an email

--- dinosaurs/js/controllers/index.js
+++ dinosaurs/js/controllers/index.js
@@ -3,6 +3,7 @@ var m = require('mithril');
 module.exports = function() {
     self = this;
 
+    self.email = m.prop('');
     self.domains = [];
     self.currentDomain = m.prop('');
 
@@ -11,6 +12,17 @@ module.exports = function() {
         m.request({method: "GET", url: "/api/v1/domains"}).then(function(ret) {
             self.domains = ret.availableDomains;
             self.currentDomain(self.domains[0]);
+        });
+    };
+
+    self.getIt = function(email) {
+        m.request({
+            method: "POST",
+            url: "/api/v1/emails",
+            data: {
+                'domain': self.currentDomain(),
+                'email': self.email()
+            }
         });
     };
 
