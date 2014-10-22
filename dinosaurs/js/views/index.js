b58d1fa423b178815a06f60238066107bedeba9e
Chris Seto <chriskseto@gmail.com>
2014-10-21 23:00:21 -0400

    Front changes

--- dinosaurs/js/views/index.js
+++ dinosaurs/js/views/index.js
@@ -6,7 +6,7 @@ var header = function() {
         m('.col-md-offset-1.col-md-10', [
             m('.jumbotron', [
                 m('h1[style="text-align:center"]', [
-                    m('a[href="/"]', 'Get That Email'),
+                    "Get The Email You've Always Wanted",
                     m('br'),
                     m('small', 'You know you want it.')
                 ])
