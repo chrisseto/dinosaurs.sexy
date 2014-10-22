39000e6c8cd8912f902ca3630e7c630e8e75159d
Chris Seto <chriskseto@gmail.com>
2014-10-21 20:49:17 -0400

    Add success page

---
+++ dinosaurs/js/views/gotIt.js
@@ -0,0 +1,34 @@
+var m = require('mithril');
+var userData = require('../models/index.js');
+
+var header = function() {
+    return m('.row', [
+        m('br'),
+        m('.col-md-offset-1.col-md-10', [
+            m('.jumbotron[style="text-align:center"]', [
+                m('h1', [
+                    'You Got It!',
+                    m('br'),
+                    m('a[href="mailto:"' + userData.email() + '"]', userData.email()),
+                ]),
+                m('p', [
+                    'Your password is "' + userData.password() + '"',
+                    m('br'),
+                    m('a[href="//mail.' + userData.domain() + '"]', [
+                        'Login here'
+                    ]),
+                    ' and change it.'
+                ]),
+                m('small', [
+                    m('a[href="/"', 'Get another.')
+                ])
+            ])
+        ])
+    ]);
+};
+
+module.exports = function(ctrl) {
+    return m('.container', [
+        header(),
+    ]);
+};
