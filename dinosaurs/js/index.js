9e5cf2f6fcbf0c2c6158f31a4376d4888542a9ee
Chris Seto <chriskseto@gmail.com>
2014-10-21 20:50:19 -0400

    Set up success route

--- dinosaurs/js/index.js
+++ dinosaurs/js/index.js
@@ -1,13 +1,19 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap/dist/js/bootstrap.js');
 
-var getThat = {};
 var m = require('mithril');
+
+var getThat = {};
+var gotThat = {};
 
 
 getThat.view = require('dinosaurs/views/index.js');
 getThat.controller = require('dinosaurs/controllers/index.js');
 
+gotThat.view = require('dinosaurs/views/gotIt.js');
+gotThat.controller = function() {};
+
 m.route(document.body, '/', {
     '/': getThat,
+    '/gotit': gotThat
 });
