17f08651b058883937de3c80e0d4bef174c95685
Chris Seto <chriskseto@gmail.com>
2014-10-30 21:24:39 -0400

    Add a bunch of exceptions

---
+++ dinosaurs/exceptions.py
@@ -0,0 +1,23 @@
+# Base Exception
+class DinosaurError(Exception):
+    pass
+
+
+class PaymentRequiredError(DinosaurError):
+    pass
+
+
+class YandexException(DinosaurError):
+    pass
+
+
+class InvalidEmailError(YandexException):
+    pass
+
+
+class InvalidDomainError(YandexException):
+    pass
+
+
+class AddressTakenError(YandexException):
+    pass
