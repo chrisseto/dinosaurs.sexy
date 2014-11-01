2fdc17b87de079b907a0009206e4c52ddbd2d930
Chris Seto <chriskseto@gmail.com>
2014-11-01 15:57:50 -0400

    Properly setup database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 import tornado.web
@@ -5,14 +6,19 @@ import tornado.ioloop
 
 from dinosaurs import views
 from dinosaurs import settings
+from dinosaurs.transaction import database
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
+    if not os.path.isfile(settings.DATABASE):
+        database.db.create_tables([database.Transaction])
+
     application = tornado.web.Application([
         (r'/api/v1/domains', views.DomainAPIHandler),
-        (r'/api/v1/emails', views.EmailAPIHandler),
+        (r'/api/v1/emails/', views.TransactionAPIHandler),
+        (r'/api/v1/emails/(.*)', views.EmailAPIHandler),
         (r'/', views.SingleStatic, {'path': 'static/index.html'}),
         (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
     ], debug=settings.DEBUG)
