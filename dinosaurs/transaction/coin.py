dafb9d31100274296a68b80b6ca12a1ebd6992af
Chris Seto <chriskseto@gmail.com>
2014-11-12 21:12:27 -0500

    Add a decorator for catching coin server related errors

--- dinosaurs/transaction/coin.py
+++ dinosaurs/transaction/coin.py
@@ -1,16 +1,31 @@
+from socket import error
+from httplib import HTTPException
+
 import dogecoinrpc
 
+from dinosaurs.exceptions import NoCoinServerError
 
 connection = dogecoinrpc.connect_to_local()
+
+
+def requires_connection(func):
+    def wraps(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (HTTPException, error):
+            raise NoCoinServerError()
+    return wraps
 
 
 def get_cost():
     return 0
 
 
+@requires_connection
 def generate_address():
     return connection.getnewaddress()
 
 
+@requires_connection
 def check_balance(addr):
     return connection.getbalance(addr)
